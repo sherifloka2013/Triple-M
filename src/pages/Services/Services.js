7d12e1ec4b4184bc526def0ad757d06c1f2fb63d
IT-SHERIFE\IT <sherif.hassan@momkn.com.eg>
2023-02-18 17:08:28 +0200

    sevices page initiated with cards for each service.

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,15 +1,39 @@
 import React  from "react";
 
-
+import './Services.css'
 const Services= () => {
   
         return (
         <div className="container">
             <h2>
-                Services Page is under Construction
+                Our Services
             </h2>
+
             
+        <div id="cardsContainer">
+            <div class="cardServ">
+                <div class="content">
+                    <h3 class="title">Solar Energy</h3>
+                    <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+                    <button class="btn">Read more</button>
+                </div>
+            </div>
+            <div class="cardServ">
+                <div class="content">
+                    <h3 class="title">Solar Energy</h3>
+                    <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+                    <button class="btn">Read more</button>
+                </div>
+            </div>
+            <div class="cardServ">
+                <div class="content">
+                    <h3 class="title">Solar Energy</h3>
+                    <p class="copy">Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains</p>
+                    <button class="btn">Read more</button>
+                </div>
+            </div>
         </div>
+     </div>
         );
 };
 
