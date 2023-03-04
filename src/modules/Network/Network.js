f29313661a686421bf1e0fbbaf48cd2dae20fd6f
IT-SHERIFE\IT <sherif.hassan@momkn.com.eg>
2023-03-04 20:58:01 +0200

    Updating Network file

---
+++ src/modules/Network/Network.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+const baseURL = "http://78.47.134.53:8055/graphql";
+axios.defaults.baseURL = baseURL;
+axios.defaults.headers = {'Content-Type': 'application/json'}
+
+
+export default {
+  about: {
+    getAboutData(body) {
+      return axios.post("",body);
+    },
+        },
+  services: {
+    getServices(body){
+      return axios.post("",body);
+          }
+        }
+}
