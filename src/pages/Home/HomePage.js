94847eac491dece33b89eb7b364bc04a5a443493
IT-SHERIFE\IT <sherif.hassan@momkn.com.eg>
2023-03-04 21:50:53 +0200

    adding timelines to Home page

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -17,17 +17,75 @@ const HomePage = () => {
                 </ul> 
                 </div>
            </section>
-           <section id="History">
-                <div>
+           
+           <section class="timeline">
+            <h3>Our History</h3>
+                <ul>
+                    <li>
+                        <div>
+                            <time>1998</time>
+                            <h1>Safa Group</h1>
+                            <p>
+                            Established in 1998 as Constructions Company
+                            Supply all kind of residential building and all kind of Infra Structure.
+                            </p>
+                            
+                        </div>
+                    </li>
+                    <li>
                     <div>
-                        <span>1998</span>
-                        <span>Icon</span>
+                        <time>2015</time>
+                        <h1>Triple M Construction</h1>
+                        <p>
+                        Established in 2015 as Renewable energy interested company
+
+                        </p>
                     </div>
+                    </li>
+                    <li>
                     <div>
-                        <p>ay kalam</p>
+                        <time>2017</time>
+                        <h1>EPSCO</h1>
+                        <p>
+                        EGYPTIAN POWER SAVING
+COMPANY established in 2017
+
+                        </p>
+                        </div>
+                    
+                    </li>
+                    <li>
+                        <div>
+                            <time>2017</time>
+                            <h1>THE EGYPTIAN JORDANIAN COMPANY FOR
+RENEWABLE ENERGY</h1>
+                            <p>
+                            Established in 2017</p>
+                            
+                        </div>
+                    </li>
+                    <li>
+                    <div>
+                        <time>2018</time>
+                        <h1>Triple M Jordan</h1>
+                        <p>Branch
+Established branch in Jordan as executive arm for Triple M construction for Renewable Energy Projects
+
+                        </p>
                     </div>
-                </div>
-           </section>
+                    </li>
+                    <li>
+                    <div>
+                        <time>2020</time>
+                        <h1>Triple M Saudi Arabia Branch</h1>
+                        <p>Established branch in KSA as executive arm for Triple M construction for Renewable Energy Projects
+
+                        </p>
+                        </div>
+                    
+                    </li>
+                </ul>
+            </section>
         </div>
         );
 };
