03543aa3a17286a07ae2d699b08ffe73f9d9301b
BooGeDe <63404142+BooGeDe@users.noreply.github.com>
2023-03-08 00:15:11 +0200

    Adding partners section to home page

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -18,8 +18,41 @@ const HomePage = () => {
                 </div>
            </section>
            
-           <section class="timeline">
-            <h3>Our History</h3>
+            <section id="partners">
+            <div className="sectionTitle">Our Partners</div>
+                <div id="partnersSlider">
+                <div className='partnerContainer'>
+                    <img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"  alt='partner'></img>
+                </div>
+                <div className='partnerContainer'>
+                <img src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"  alt='partner'></img>
+                </div>
+                <div className='partnerContainer'>
+                <img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"  alt='partner'></img>
+                </div>
+                <div className='partnerContainer'>
+                    <img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg"  alt='partner'></img>
+                </div>
+                <div className='partnerContainer'>
+                <img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg"  alt='partner'></img>
+                </div>
+                <div className='partnerContainer'>
+                <img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"  alt='partner'></img>
+                </div>
+                <div className='partnerContainer'>
+                    <img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"  alt='partner'></img>
+                </div>
+                <div className='partnerContainer'>
+                <img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"  alt='partner'></img>
+                </div>
+                <div className='partnerContainer'>
+                <img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"  alt='partner'></img>
+                </div>
+                </div>
+            </section>
+
+            <section className="timeline">
+            <div className="sectionTitle">Our History</div>
                 <ul>
                     <li>
                         <div>
@@ -87,17 +120,6 @@ Established branch in Jordan as executive arm for Triple M construction for Rene
                 </ul>
             </section>
 
-            <section id="partners">
-                <div class='partnerContainer'>
-                    <img src='' alt='partner'/>
-                </div>
-                <div class='partnerContainer'>
-                    <img src='' alt='partner'/>
-                </div>
-                <div class='partnerContainer'>
-                    <img src='' alt='partner'/>
-                </div>
-            </section>
         </div>
         );
 };
